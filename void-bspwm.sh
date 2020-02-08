5c13bdbd756493cc3f45bdbe35de14dd9c6c326b
Marcilio Nascimento <marcilio.mcn@gmail.com>
2020-02-08 13:02:32 -0300

    Adjusts in Firewall test and bspwm configs

--- void-bspwm.sh
+++ void-bspwm.sh
@@ -24,15 +24,16 @@ sudo xbps-install -y $PKG_LIST
 
 # Configure bspwm and sxhkd start (disable after customize dotfiles for bspwm and sxhkd)
 mkdir -p ~/.config/{bspwm,sxhkd}
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 chmod +x ~/.config/bspwm/bspwmrc
-sed 's/urxvt/st/' < ~/.config/sxhkd/sxhkdrc
+#sed 's/urxvt/st/' < ~/.config/sxhkd/sxhkdrc
 echo 'exec bspwm' > ~/.xinitrc
 # Configure bspwm and sxhkd end
 
 # Configure Firewall if 1
-if [ $FIREWALL ]; then
+if [ $FIREWALL ]
+then
 	sudo xbps-reconfigure ufw
 	sudo ufw enable
 	sudo ln -s /etc/sv/ufw /var/service
