5797cad74c05d3dc181f5f47eefc47885386e2a3
Marcilio Nascimento <marcilio.mcn@gmail.com>
2020-02-08 14:33:33 -0300

    Firewll tests and sed -i parameter

--- void-bspwm.sh
+++ void-bspwm.sh
@@ -17,23 +17,22 @@ PKG_LIST='xorg-minimal terminus-font freefont-ttf xset alsa-utils bspwm sxhkd st
 FIREWALL=0 # 1=Install, 0=Do not install (install only if running some server daemon)
 
 # If Firewall is 1
-[ $FIREWALL ] && PKG_LIST+=' ufw'
+[ $FIREWALL -eq 1 ] && PKG_LIST+=' ufw'
 
 # Install packages
 sudo xbps-install -y $PKG_LIST
 
 # Configure bspwm and sxhkd start (disable after customize dotfiles for bspwm and sxhkd)
 mkdir -p ~/.config/{bspwm,sxhkd}
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd
 chmod +x ~/.config/bspwm/bspwmrc
-#sed 's/urxvt/st/' < ~/.config/sxhkd/sxhkdrc
+sed -i 's/urxvt/st/' ~/.config/sxhkd/sxhkdrc
 echo 'exec bspwm' > ~/.xinitrc
 # Configure bspwm and sxhkd end
 
 # Configure Firewall if 1
-if [ $FIREWALL ]
-then
+if [ $FIREWALL -eq 1 ]; then
 	sudo xbps-reconfigure ufw
 	sudo ufw enable
 	sudo ln -s /etc/sv/ufw /var/service
